15c38b0e9e5e5989e271eb6b7be55c4aeeb8fdef
François Pelsser <github@frol.be>
2012-03-20 11:15:59 +0100

    cleaned

--- LINGI2347-computer_system_security/Challenge3/md5i.c
+++ LINGI2347-computer_system_security/Challenge3/md5i.c
@@ -4,37 +4,44 @@
 #include <openssl/md5.h> 
 
 
-unsigned char md5(int p)
+void int32_to_char(int i, char * c)
 {
+    int j;
+    for(j =0; j<4; j++)
+        c[j] = (i>>(8*j))  & 0xff;
+}
+
+void md5_iterate(char * p, int plength, int niter, unsigned char* out)
+{
+    int i;
+    for(i=0;i<niter;i++)
+    {
+        if(i == 0)
+        {
+            MD5(p, plength,out); 
+        }
+        else
+        {
+            MD5(out, MD5_DIGEST_LENGTH,out); 
+        }
+    }
+}
+
+void print_hex(unsigned char* h,int length)
+{
+    int i;
+    for(i=0;i<length;i++)
+        printf("%02x",h[i]);
     
 }
 
-
 int main() { 
-int i;
-int har = 123456789;
-char* a = malloc(sizeof(char) *4);
-a[0] = har & 0xff;
-a[1] = (har>>8)  & 0xff;
-a[2] = (har>>16) & 0xff;
-a[3] = (har>>24) & 0xff;
-//printf("a length: %d\n",strlen(a));
-//printf("a : %d\n",a[1]);
-//printf("a : %d\n",a[2]);
-//printf("a : %d\n",a[3]);
-//printf("a : %d\n",a[4]);
-//printf("a : %s\n",a);
-unsigned char md1[MD5_DIGEST_LENGTH];
-unsigned char md2[MD5_DIGEST_LENGTH];
-unsigned char md3[MD5_DIGEST_LENGTH];
-unsigned char md4[MD5_DIGEST_LENGTH];
-unsigned char md5[MD5_DIGEST_LENGTH];
-MD5(a, 4,md1); 
-MD5(md1, 16,md2); 
-MD5(md2, 16,md3); 
-MD5(md3, 16,md4); 
-MD5(md4, 16,md5); 
-for(i=0;i<MD5_DIGEST_LENGTH;i++)
-    printf("%02x",md5[i]);
-return 0; 
+    int har = 123456789;
+    char a[4];
+    unsigned char out[MD5_DIGEST_LENGTH];
+    int32_to_char(har,a);
+    md5_iterate(a,4,5,out);
+    print_hex(out,16);
+    printf("\n");
+    return 0; 
 }
